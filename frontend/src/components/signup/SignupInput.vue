4ba30837362e353132254918a3d3781e092ac78d
daep93 <eoy1313@gmail.com>
2021-01-14 12:57:40 +0900

    리팩토링 2

---
+++ frontend/src/components/signup/SignupInput.vue
@@ -0,0 +1,52 @@
+<template>
+  <div style="height:100%">
+    <div class="row justify-end" style="height:15%">
+      <q-icon
+        :name="$i.ionClose"
+        class="q-ma-lg"
+        size="md"
+        @click="offModal"
+      ></q-icon>
+    </div>
+    <div class="row justify-start " style="height:10%">
+      <p class="text-h4 text-weight-bolder">
+        계정 생성
+      </p>
+    </div>
+    <div class="row" style="height:55%">
+      <signup-form></signup-form>
+    </div>
+
+    <!-- SNS로 가입하기 -->
+    <div class="row justify-around q-mt-md" style=" width:400px;">
+      <q-btn size="16px" round color="blue-8">
+        <q-icon name="img:google.svg"></q-icon>
+      </q-btn>
+      <q-btn size="16px" round color="black">
+        <q-icon name="img:github.svg"></q-icon>
+      </q-btn>
+      <q-btn size="16px" round style="background-color:#1877F2">
+        <q-icon name="img:facebook.svg"></q-icon>
+      </q-btn>
+      <q-btn size="16px" round color="yellow">
+        <q-icon name="img:kakao.svg" size="45px"></q-icon>
+      </q-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+import SignupForm from '@/components/signup/SignupForm.vue';
+export default {
+  components: {
+    SignupForm,
+  },
+  methods: {
+    offModal() {
+      this.$store.commit('offAccountModal');
+    },
+  },
+};
+</script>
+
+<style scoped></style>
--- frontend/src/components/signup/SignupModal.vue
+++ frontend/src/components/signup/SignupModal.vue
@@ -11,50 +11,7 @@
         </div>
       </div>
       <div class="col-5">
-        <div class="row justify-end" style="height:15%">
-          <q-icon
-            :name="$i.ionClose"
-            class="q-ma-lg"
-            size="md"
-            @click="offModal"
-          ></q-icon>
-        </div>
-        <div class="row justify-start " style="height:10%">
-          <p class="text-h4 text-weight-bolder">
-            계정 생성
-          </p>
-        </div>
-        <q-form @submit.prevent="" style="height:60%">
-          <div class="row" style="height:80%">
-            <!-- 회원가입 입력 form -->
-            <signup-form></signup-form>
-          </div>
-          <!-- Signup 버튼  -->
-          <div class="row" style="height:20%">
-            <q-btn
-              color="blue-12"
-              class="text-center"
-              style="width:400px;height:50px;border-radius:5px;font-size:16pt"
-              type="submit"
-              >회원가입</q-btn
-            >
-          </div>
-        </q-form>
-        <!-- SNS로 가입하기 -->
-        <div class="row justify-around " style=" width:400px">
-          <q-btn size="16px" round color="blue-8">
-            <q-icon name="img:google.svg"></q-icon>
-          </q-btn>
-          <q-btn size="16px" round color="black">
-            <q-icon name="img:github.svg"></q-icon>
-          </q-btn>
-          <q-btn size="16px" round style="background-color:#1877F2">
-            <q-icon name="img:facebook.svg"></q-icon>
-          </q-btn>
-          <q-btn size="16px" round color="yellow">
-            <q-icon name="img:kakao.svg" size="45px"></q-icon>
-          </q-btn>
-        </div>
+        <signup-input></signup-input>
       </div>
     </div>
   </account-modal>
@@ -62,14 +19,10 @@
 
 <script>
 import AccountModal from '@/components/common/AccountModal.vue';
-import SignupForm from '@/components/signup/SignupForm.vue';
+import SignupInput from '@/components/signup/SignupInput.vue';
+
 export default {
-  components: { AccountModal, SignupForm },
-  methods: {
-    offModal() {
-      this.$store.commit('offAccountModal');
-    },
-  },
+  components: { AccountModal, SignupInput },
   computed: {},
 };
 </script>
