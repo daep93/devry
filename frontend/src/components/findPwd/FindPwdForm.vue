35d475d0a0c72a87a6affca274639cbaec4a70b1
vreez <dearsoou@gmail.com>
2021-01-14 14:57:20 +0900

    change password 페이지 UI 구현

---
+++ frontend/src/components/findPwd/FindPwdForm.vue
@@ -0,0 +1,72 @@
+<template>
+  <div style="width:400px; height:665px; margin: 0 auto;">
+    <q-icon
+      :name="$i.ionCloseOutline"
+      class="close-button"
+      style="width:30px; height:30px; margin-left: 370px"
+    >
+    </q-icon>
+    <div class="title text-h6 text-weight-bold">Create new password</div>
+    <q-input
+      class="input"
+      v-model="newPwd"
+      type="password"
+      label="password"
+      placeholder="6-12자 이내의 새로운 비밀번호를 입력해주세요"
+      style="width:400px;"
+      stack-label
+      :dense="dense"
+    />
+    <q-input
+      class="input"
+      v-model="confirmNewPwd"
+      type="password"
+      label="confirm password"
+      placeholder="비밀번호를 다시 한 번 입력해주세요"
+      style="width:400px;"
+      stack-label
+      :dense="dense"
+    />
+    <q-btn
+      :disabled="newPwd.length > 15 || newPwd.length < 6"
+      class="save-button"
+      color="primary"
+      label="SAVE"
+      style="width:400px; height:50px;"
+      type="submit"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      password: '',
+      newPwd: '',
+      confirmNewPwd: '',
+      dense: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+.title {
+  margin-bottom: 30px;
+  padding-top: 30%;
+}
+
+.close-button {
+  padding-top: 30px;
+}
+
+.input {
+  margin-bottom: 25px;
+}
+
+.save-button {
+  margin-top: 80px;
+}
+
+</style>
--- frontend/src/components/findPwd/FindPwdModal.vue
+++ frontend/src/components/findPwd/FindPwdModal.vue
@@ -1,18 +1,27 @@
 <template>
   <account-modal>
-    <div class="row">
+    <div class="q-pa-md row">
       <!-- 모달 이미지 -->
-      <div class="col-6">FindPwd img</div>
+      <div class="col-6 row justify-center flex items-center">
+        <q-img 
+          :src="require('@/assets/change_pwd.png')" 
+          alt="change-password" 
+          style="width:552px; heigth:680px; vertical-align:middle;"
+        />
+      </div>
       <!-- 모달 컨텐츠 -->
-      <div class="col-6">FindPwd contents</div>
+      <div class="col-6">
+        <find-pwd-form></find-pwd-form>
+      </div>
     </div>
   </account-modal>
 </template>
 
 <script>
 import AccountModal from '@/components/common/AccountModal';
+import FindPwdForm from './FindPwdForm.vue';
 export default {
-  components: { AccountModal },
+  components: { AccountModal, FindPwdForm },
 };
 </script>
 
