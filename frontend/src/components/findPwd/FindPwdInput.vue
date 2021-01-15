3b4ac164f0b4ed13a7a93cf58111aeb004bdd17a
vreez <dearsoou@gmail.com>
2021-01-15 11:49:11 +0900

    nickname 입력창 추가

--- frontend/src/components/findPwd/FindPwdInput.vue
+++ frontend/src/components/findPwd/FindPwdInput.vue
@@ -8,30 +8,50 @@
     >
     </q-icon>
     <div class="title text-h6 text-weight-bold">Forgot password</div>
-    <q-input
-      class="input"
-      v-model="email"
-      type="email"
-      label="email"
-      label-slot
-      placeholder="회원가입시 사용하신 이메일을 입력해주세요"
-      style="width:400px;"
-      stack-label
-      :dense="dense"
-      :error="!isValidEmail"
-    >
-      <template v-slot:label>
-        <h7>email</h7>
-        <br />
-      </template>
-      <template v-slot:error>
-        <div class="text-red-8">
-          잘못된 이메일 양식입니다.
-        </div>
-      </template>
-    </q-input>
-
+    <div class="row" style="width:400px">
+      <div class="col-8">
+        <q-input
+          class="input"
+          v-model="email"
+          type="email"
+          label="email"
+          label-slot
+          placeholder="이메일을 입력해주세요"
+          style="width:250px;"
+          stack-label
+          :dense="dense"
+          :error="!isValidEmail"
+        >
+          <template v-slot:label>
+            <h7>email</h7>
+            <br />
+          </template>
+          <template v-slot:error>
+            <div class="text-red-8">
+              잘못된 이메일 양식입니다.
+            </div>
+          </template>
+        </q-input>
+      </div>  
+      <div class="col-1"></div>
+      <div class="col-3">
+        <q-input
+          v-model="nickname"
+          clearable
+          :clear-icon="$i.ionCloseOutline"
+          stack-label
+          label-slot
+          bottom-slots
+        >
+          <template v-slot:label>
+            <h7>별명</h7>
+            <br />
+          </template>
+        </q-input>
+      </div>
+    </div>  
     <q-btn
+      @click="moveToChangePwd"
       :disabled="!checkForm"
       class="save-button"
       color="primary"
@@ -71,6 +91,7 @@ export default {
       try {
         // await checkUser({
         //   email: this.email,
+        //   username: this.nickname
         // });
         alert('이메일을 보냈습니다. 비밀번호 변경을 진행해주세요')
         // this.$emit('findPwd');
@@ -79,14 +100,12 @@ export default {
         console.log(error);
         console.log('error발생')
       }
+    },
+    moveToChangePwd() {
+      this.$store.commit('setAccountModalType', '')
     }
   },
   created() {
-    // 로그인 되어있는 상태인지 확인
-    // const token = localStorage.getItem('jwt')
-    // if (token) {
-    //   this.login = true
-    // }
   }
 }
 </script>
