7f03d9eb0ef4e2a447dfff3be6e6d87a4b4c4f84
vreez <dearsoou@gmail.com>
2021-01-19 17:43:29 +0900

    [S04P12B102-31] docs/Dev_log : Create Common 0119 log

---
+++ frontend/src/components/findPwd/FindPwdFail.vue
@@ -0,0 +1,33 @@
+<template>
+  <div style="height:50%">
+    <div class="row justify-start items-end" style="height:60%">
+      <p class="text-h4 text-weight-bolder">
+        비밀번호 변경 실패
+      </p>
+    </div>
+    <br />
+    <br />
+    <div class="row justify-start" style="height:55%">
+      <q-btn
+        color="blue-12"
+        class="text-center"
+        style="width:330px;height:50px;border-radius:5px;font-size:16pt"
+        type="submit"
+        @click="backToReset"
+        >뒤로가기</q-btn
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    backToReset() {
+      this.$emit('authPwdSuccess');
+    },
+  },
+};
+</script>
+
+<style scoped></style>
