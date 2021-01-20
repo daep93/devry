5e605c7035fa752f3137c892de3e76b3f4f6ec87
applevalley <skatkd7571@naver.com>
2021-01-20 19:44:02 +0900

    [S04P12B102-33] feat/email : Create email_validation

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,9 +1,90 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+from django.utils.translation import ugettext_lazy as _
+
+from django.dispatch import receiver
+from django.urls import reverse
+from django.template.loader import render_to_string
+from django_rest_passwordreset.signals import reset_password_token_created
+from django.core.mail import EmailMultiAlternatives
+
 # Create your models here.
 
+class UserManager(BaseUserManager):
 
-class User(AbstractUser):
-    email = models.EmailField('email', unique=True)
-    username = models.CharField(max_length=150, unique=True)
+    def _user_create(self, email, password, **extra_fields): 
+        if not email:
+            raise ValueError('input email')
+        user = self.model(
+            email = self.normalize_email(email),
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using = self._db)
+        return user
 
+    def user_create(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._user_create(email, password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(
+        verbose_name='email',
+        max_length=80,
+        unique=True
+    )
+    username = models.CharField(
+        verbose_name='username',
+        max_length=30,
+        unique=True
+    )
+    is_active = models.BooleanField(
+        verbose_name='is active',
+        default=True
+    )
+    date_joined = models.DateTimeField(
+        _('date joined'),
+        default=timezone.now
+    )
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username',]
+
+    def __str__(self):
+        return self.username
+
+    # email = models.EmailField('email', unique=True)
+    # username = models.CharField(max_length=20, unique=True)
+
+
+@receiver(reset_password_token_created)
+def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
+
+    context = {
+        'current_user': reset_password_token.user,
+        'username': reset_password_token.user.username,
+        'email': reset_password_token.user.email,
+        'reset_password_url': "{}?token={}".format(
+            instance.request.build_absolute.uri(reverse('password_reset:reset-password-confirm')),
+            reset_password_token.key)
+    }
+
+    email_html_message = render_to_string('email/user_reset_password.html', context)
+    email_plaintext_message = render_to_string('email/user_reset_password.txt', context)
+
+    msg = EmailMultiAlternatives(
+        # title:
+        "Password Reset for {title}".format(title="Some website title"),
+        # message:
+        email_plaintext_message,
+        # from:
+        "noreply@somehost.local",
+        # to:
+        [reset_password_token.user.email]
+    )
+    msg.attach_alternative(email_html_message, "text/html")
+    msg.send()
