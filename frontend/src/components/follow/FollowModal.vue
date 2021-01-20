5ce632d0426ba694850d19cb1de251300c05c9f7
sieun <najse77@naver.com>
2021-01-21 00:38:28 +0900

    [S04P12B102-36] feat/Follow : Design follow modal page

--- frontend/src/components/follow/FollowModal.vue
+++ frontend/src/components/follow/FollowModal.vue
@@ -1,13 +1,86 @@
 <template>
   <q-dialog v-model="$store.state.followModal">
-    <q-card style="min-width:900px; height:700px">
-      <p>aa</p>
+    <q-card style="min-width:650px; height:670px">
+      <!-- x버튼 -->
+      <div class="row justify-end" style="height:40px;">
+        <q-icon
+          :name="$i.ionClose"
+          class="q-ma-lg"
+          size="md"
+          @click="offModal"
+        ></q-icon>
+      </div>
+
+      <!-- 스크롤 영역 설정 -->
+      <div class="q-ma-md">
+        <q-scroll-area
+          :thumb-style="thumbStyle"
+          style="height: 585px; max-width: 650px;"
+        >
+
+        <!-- 탭 시작 -->
+          <div class="q-pa-md">
+            <div class="q-gutter-y-md" style="max-width: 600px">
+                <center>
+                <q-tabs
+                  v-model="tab"
+                  class="text-grey"
+                  active-color="primary"
+                  indicator-color="primary"
+                  align="justify"
+                  style="max-width: 450px;"
+                >
+                  <q-tab name="follow" label="팔로워" />
+                  <q-tab name="following" label="팔로잉" />
+                </q-tabs>
+
+                <q-separator style="width: 75%;" />
+              </center>
+
+              <q-tab-panels v-model="tab" animated>
+                <q-tab-panel name="follow" style="padding-top: 0px;">
+                  <FollowerList></FollowerList>
+                </q-tab-panel>
+
+                <q-tab-panel name="following" style="padding-top: 0px;">
+                  <FollowingList></FollowingList>
+                </q-tab-panel>
+              </q-tab-panels>
+
+            </div>
+          </div>
+
+        </q-scroll-area>
+      </div>
+
     </q-card>
   </q-dialog>
 </template>
 
 <script>
-export default {};
+import FollowerList from '@/components/follow/FollowerList';
+import FollowingList from '@/components/follow/FollowingList';
+export default {
+  components: { FollowerList, FollowingList },
+  data() {
+    return {
+      tab: 'follow',
+      thumbStyle: {
+        right: '2px',
+        borderRadius: '5px',
+        backgroundColor: '#027be3',
+        width: '5px',
+        opacity: 0.75,
+      },
+    };
+  },
+  methods: {
+    offModal() {
+      this.$store.commit('offFollowModal');
+    },
+  },
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+</style>
---
+++ frontend/src/components/follow/FollowerList.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+
+    <div v-for="n in 10" :key="n" class="q-pa-xs">
+      <div class="q-pa-md row col-align" style="height:80px;">
+        <!-- follow 유저 정보 -->
+        <div class="col-8 row" style="height:100%">
+          <q-list style="min-width:300px; margin-left: 30px;">
+            <!-- <q-list bordered style="min-width:300px; margin-left: 30px;"> -->
+            <q-item>
+              <q-item-section avatar>
+                <q-avatar>
+                  <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+                </q-avatar>
+              </q-item-section>
+              <q-item-section>
+                <span><b style="font-size: 15px;">유저 이름</b></span>
+                <span>글 0 · 팔로워 0</span>
+              </q-item-section>
+
+            </q-item>
+          </q-list>
+        </div>
+        <!-- follow 버튼 -->
+        <div class="col-4 row justify-center items-center" style="height:100%">
+          <q-btn no-caps color="primary" id="follow-btn" label="Follow" style="width:90px;" />
+        </div>
+      </div>
+      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.col-align {
+  display: flex;
+  align-items: center;
+}
+
+</style>
