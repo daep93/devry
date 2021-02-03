78a124180eded0e8b15c0e3bb169edeeb9cbedc5
sieun <najse77@naver.com>
2021-02-04 01:51:37 +0900

    [S04P13B102-48] feat/QnaDetail : Add comment adoption on QnaDetailComment

--- frontend/src/components/qna/QnaBigComment.vue
+++ frontend/src/components/qna/QnaBigComment.vue
@@ -1,84 +1,5 @@
 <template>
   <div class="row col-12">
-    <!-- 첫 번째 댓글 -->
-    <div class="row col-12">
-      <!-- 댓글 좋아요 수, 댓글 수, 북마크 수 -->
-      <div class="row col-2">
-        <div class="row col-9"></div>
-        <div class="row col-3 q-mt-lg">
-          <qna-comment-status></qna-comment-status>
-        </div>
-      </div>
-      <div class="row col-10">
-        <div class="row col-9">
-          <q-card flat bordered class="my-card q-pa-lg q-mt-lg row col-12">
-            <div class="row col-9">
-              <div class="q-ml-md row col-12 q-my-sm">
-                <div style="color: blue">
-                  @Hello
-                </div>
-                <span class="q-ml-sm text-caption" style="color: gray;">
-                  50분 전
-                </span>
-              </div>
-            </div>
-            <div v-if="!blueModel" class="row col-2">
-              <div
-                class="q-pl-xl q-mt-sm"
-                v-for="(color, msg) in {
-                  '답변 채택': '#1976D2',
-                }"
-                :key="msg"
-              >
-                <div
-                  class="row col-12 shadow-1 overflow-hidden"
-                  style="border-radius:5px; width: 100px; height: 20px;"
-                >
-                  <div
-                    class="col-2"
-                    :style="{ 'background-color': color }"
-                  ></div>
-                  <div class="col-10 text-center">{{ msg }}</div>
-                </div>
-              </div>
-            </div>
-            <div v-else class="row col-2"></div>
-            <div class="row col-1 q-mb-sm q-pl-sm">
-              <q-toggle
-                :label="채택하기"
-                :true-value="false"
-                :false-value="true"
-                v-model="blueModel"
-              />
-            </div>
-            <div class="q-ml-md row col-12">
-              <div class="q-mr-md q-pb-lg">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Exercitationem quidem, iure eveniet praesentium repudiandae
-                necessitatibus delectus explicabo repellat id eligendi corrupti.
-                Dolorem harum, nihil ipsum accusantium delectus labore dolorum,
-                illo
-              </div>
-            </div>
-            <div class="row col-12 q-px-md">
-              <q-separator />
-            </div>
-            <div class="q-mt-sm q-px-md row col-12">
-              <q-input
-                borderless
-                v-model="text"
-                placeholder="댓글을 입력해주세요"
-              />
-            </div>
-          </q-card>
-        </div>
-        <!-- 채택 댓글 프로필 -->
-        <div v-if="!blueModel">
-          <qna-comment-selected></qna-comment-selected>
-        </div>
-      </div>
-    </div>
-
     <!-- 두 번째 댓글 -->
     <div class="row col-12">
       <div class="row col-2">
@@ -101,34 +22,74 @@
                 </span>
               </div>
             </div>
-            <div v-if="!blueModel" class="row col-2">
-              <div
-                class="q-pl-xl q-mt-sm"
-                v-for="(color, msg) in {
-                  '답변 채택': '#1976D2',
-                }"
-                :key="msg"
-              >
+            <!-- 토글이 꺼져있으며, 글 작성자가 아닌 경우 -->
+            <div v-if="blueModel && !writerStatus" class="row col-3"></div>
+            <!-- 토글이 켜져있으며 글 작성자가 아닌 경우 -->
+            <div v-else-if="!blueModel && !writerStatus" class="row col-3">
+              <div class="row col-3 q-mb-sm q-pl-sm"></div>
+              <div class="row col-9">
                 <div
-                  class="row col-12 shadow-1 overflow-hidden"
-                  style="border-radius:5px; width: 100px; height: 20px;"
+                  class="q-pl-xl q-mt-sm"
+                  v-for="(color, msg) in {
+                    '답변 채택': '#1976D2',
+                  }"
+                  :key="msg"
                 >
                   <div
-                    class="col-2"
-                    :style="{ 'background-color': color }"
-                  ></div>
-                  <div class="col-10 text-center">{{ msg }}</div>
+                    class="row col-12 shadow-1 overflow-hidden"
+                    style="border-radius:5px; width: 100px; height: 20px;"
+                  >
+                    <div
+                      class="col-2"
+                      :style="{ 'background-color': color }"
+                    ></div>
+                    <div class="col-10 text-center">{{ msg }}</div>
+                  </div>
                 </div>
               </div>
             </div>
-            <div v-else class="row col-2"></div>
-            <div class="row col-1 q-mb-sm q-pl-sm">
-              <q-toggle
-                :label="채택하기"
-                :true-value="false"
-                :false-value="true"
-                v-model="blueModel"
-              />
+            <!-- 토글이 꺼져있으며 글 작성자가 맞는 경우 -->
+            <div v-else-if="blueModel && writerStatus" class="row col-3">
+              <div class="row col-8"></div>
+              <div class="row col-4 q-mb-sm q-pl-sm">
+                <q-toggle
+                  :label="채택하기"
+                  :true-value="false"
+                  :false-value="true"
+                  v-model="blueModel"
+                />
+              </div>
+            </div>
+            <!-- 토글이 켜져있으며, 글 작성자가 맞는 경우 -->
+            <div v-else-if="!blueModel && writerStatus" class="row col-3">
+              <div class="row col-8">
+                <div
+                  class="q-pl-xl q-mt-sm"
+                  v-for="(color, msg) in {
+                    '답변 채택': '#1976D2',
+                  }"
+                  :key="msg"
+                >
+                  <div
+                    class="row col-12 shadow-1 overflow-hidden"
+                    style="border-radius:5px; width: 100px; height: 20px;"
+                  >
+                    <div
+                      class="col-2"
+                      :style="{ 'background-color': color }"
+                    ></div>
+                    <div class="col-10 text-center">{{ msg }}</div>
+                  </div>
+                </div>
+              </div>
+              <div class="row col-4 q-mb-sm q-pl-sm">
+                <q-toggle
+                  :label="채택하기"
+                  :true-value="false"
+                  :false-value="true"
+                  v-model="blueModel"
+                />
+              </div>
             </div>
             <div class="q-ml-md row col-12">
               <div class="q-mr-md q-pb-lg">
@@ -179,12 +140,46 @@ export default {
   },
   data() {
     return {
-      title: 'Add a YouTube stats widget to your iPhone with JavaScript',
-      username: 'Test User',
-      profile_img: 'https://cdn.quasar.dev/img/avatar.png',
       follow: true,
       blueModel: true,
+      writerStatus: false,
+      QnaDetailData: {
+        user_info: {
+          userid: 1,
+          username: 'SSAFY PARK',
+          profile_img: 'user/1',
+          post_num: 5,
+          follower_num: 3,
+          bio: 'Hello DEVRY! Nice Day!!',
+          pinned: [
+            {
+              title: '2021 Awesome Tech Top 5',
+              post_id: 310,
+            },
+            {
+              title: '2020 Awesome Tech Top 3', // pinned한 게시글의 제목
+              post_id: 4, // pinned한 게시글의 번호
+            },
+            {
+              title: 'How to test your project efficiently', // pinned한 게시글의 제목
+              post_id: 203, // pinned한 게시글의 번호
+            },
+          ],
+          liked: true,
+          is_following: true,
+        },
+      },
     };
+  },
+  methods: {
+    checkWriter() {
+      // 글 작성자 판별
+      if (this.$store.state.id === this.QnaDetailData.user_info.userid) {
+        this.writerStatus = true;
+      }
+      // console.log('1번', this.$store.state.id);
+      // console.log('2번', this.QnaDetailData.user_info.userid);
+    },
   },
 };
 </script>
--- frontend/src/components/qna/QnaDetailContent.vue
+++ frontend/src/components/qna/QnaDetailContent.vue
@@ -91,6 +91,93 @@ export default {
     return {
       msg: true,
       tags: ['Vue', 'Javascript', 'Typescript'],
+      writerStatus: false,
+      QnaDetailData: {
+        user_info: {
+          userid: 1,
+          username: 'SSAFY PARK',
+          profile_img: 'user/1',
+          post_num: 5,
+          follower_num: 3,
+          bio: 'Hello DEVRY! Nice Day!!',
+          pinned: [
+            {
+              title: '2021 Awesome Tech Top 5',
+              post_id: 310,
+            },
+            {
+              title: '2020 Awesome Tech Top 3', // pinned한 게시글의 제목
+              post_id: 4, // pinned한 게시글의 번호
+            },
+            {
+              title: 'How to test your project efficiently', // pinned한 게시글의 제목
+              post_id: 203, // pinned한 게시글의 번호
+            },
+          ],
+          liked: true,
+          is_following: true,
+        },
+        quest_post: {
+          title: 'How can I use axios in Vue.js?', // 질문 제목
+          written_time: '2021-01-24T02:02', // 글 작성시간
+          ref_tags: ['vue', 'javascript'], // 참조한 태그들
+          solved: true, // 해결 여부
+          like_num: 3, // 좋아요 횟수
+          comment_num: 2, // 댓글 갯수
+          viewed_num: 300, // 조회 수
+          bookmarked_num: 1, // 북마크된 횟수
+          contents:
+            'One of the most essential parts of frontend development is communication with the backend by making HTTP requests. There are a few ways how we can make API calls in Javascript asynchronously.', // 질문 글 (우선은 단순 string으로 취급)
+          answers: [
+            {
+              // 작은 댓글들 (시간순으로 배열에 저장됨)
+              contents:
+                'Vue.js in the official docs indirectly explains the mechanism of the key special attribute in vue.', // 답변 글(단순 string)
+              userid: 45, // 댓글 작성자 id 번호
+              username: 'SSAFY MAN', // 댓글 작성자 닉네임
+              written_time: '2021-01-25T03:02', // 댓글 작성시간
+            },
+            {
+              // 작은 댓글들 (시간순으로 배열에 저장됨)
+              contents:
+                'Good Sunday, welcome to my series about destructuring one of those frequently shared JavaScript quizzes on Twitter.', // 답변 글(단순 string)
+              userid: 265, // 댓글 작성자 id 번호
+              username: 'Newbie in SSAFY', // 댓글 작성자 닉네임
+              written_time: '2021-01-25T05:23', // 댓글 작성시간
+            },
+          ],
+        },
+        answer_posts: [
+          {
+            user_info: {
+              // 큰 댓글 작성자 정보
+              userid: 5, // id 번호
+              username: 'SSAFY ONE', // 닉네임
+              written_time: '2021-01-25T05:23', // 글 작성 시간
+              profile_img: 'user/5', // 프로필 사진 url
+              post_num: 3, // 작성한 포스트 횟수
+              follower_num: 10, // 팔로워 수
+              bio: 'SSAFY is The One!', // 유저 소개
+            },
+            quest_post: {
+              title: 'Read vue documentation', // 질문 제목
+              assisted: true, // 답변 채택 여부
+              like_num: 300, // 좋아요 횟수
+              comment_num: 1, // 댓글 갯수
+              contents: 'Just Read it plz.', // 질문 글
+              answers: [
+                {
+                  // 작은 댓글들 (시간순으로 배열에 저장됨)
+                  contents: 'I agree', // 답변 글(단순 string)
+                  userid: 2, // 댓글 작성자 id 번호
+                  username: 'SSAFY Pro', // 댓글 작성자 닉네임
+                  written_time: '2021-01-25T05:26', // 댓글 작성시간
+                },
+              ],
+            },
+          },
+        ],
+      },
     };
   },
   methods: {
