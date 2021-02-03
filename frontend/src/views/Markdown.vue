d7ad93e577ca9adfa34966527b720a8ea43dccae
김윤빈 <kss759153@naver.com>
2021-02-03 10:25:35 +0900

    [S04P13B102-39] feat/QnaBoardCreate : Update qna board create page markdown

--- frontend/src/views/Markdown.vue
+++ frontend/src/views/Markdown.vue
@@ -1,25 +1,40 @@
 <template>
-  <div>
-    <!-- TODO : markdown형식으로 입력받고 출력하는 것 연습하기 -->
-    <q-markdown>
-      # hello
-      <!-- <textarea name="" id="" cols="30" rows="10"><q-markdown></q-markdown></textarea> -->
-    </q-markdown>
-    <!-- <textarea v-model="contents" name="" id="" cols="30" rows="10"></textarea> -->
-    <div v-html="contents"></div>
+  <div class="q-pa-md q-gutter-sm row justify-center">
+    <div class="q-pa-sm" style="width: 80vw;">
+      <!-- 타이틀 -->
+      <div class="q-mb-md">
+        <span class="text-h4 text-weight-bolder">QnA Registration</span>
+        <p class="text-subtitle2 q-mt-md q-mb-xl" >궁금하신 점을 질문해보세요:)</p>
+      </div>
+      <!-- 마크다운 에디터 -->
+      <v-md-editor v-model="text" height="400px"></v-md-editor>
+    </div>  
   </div>
 </template>
 
 
 <script>
+import Vue from 'vue';
+import VMdEditor from '@kangc/v-md-editor';
+import '@kangc/v-md-editor/lib/style/base-editor.css';
+import githubTheme from '@kangc/v-md-editor/lib/theme/github.js';
+import '@kangc/v-md-editor/lib/theme/style/github.css';
+// import enUS from '@kangc/v-md-editor/lib/lang/en-US';
+
+// VueMarkdownEditor.lang.use('en-US', enUS);
+
+VMdEditor.use(githubTheme);
+
+Vue.use(VMdEditor);
+
 export default {
   data() {
     return {
-      contents: ''
-    }
-  }
+      text: '',
+    };
+  },
+  
 }
-
 </script>
 
 <style>
