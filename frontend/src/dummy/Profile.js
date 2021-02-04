a64fe61979884a039e3887deb5ca148c45ade722
daep93 <eoy1313@gmail.com>
2021-02-04 14:27:51 +0900

    [] feat/Profile : Create profile dummy dataa

---
+++ frontend/src/dummy/Profile.js
@@ -0,0 +1,118 @@
+const testCase = {
+  email: 'ssafyPark@edu.ssafy.com',
+  username: 'Ssafy Park',
+  joined: '2013-02-08',
+  follower_num: 4301,
+  followee_num: 392,
+  region: 'Daejeon',
+  group: 'Multi Campus',
+  bio:
+    "Jumped into web development 1st of January 2020 and I'm completely in love. I have little experience as a blogger but practice makes perfect! 🤖🦾",
+  links: {
+    Github: 'https://github.com/daep93/',
+    Gitlab: 'https://lab.ssafy.com/',
+    Facebook: 'https://www.facebook.com/groups/vuejs.korea/',
+    Linkedin:
+      'https://www.linkedin.com/in/%EB%8C%80%ED%98%84-%EB%B0%95-001319202/',
+  },
+  tech_stacks: ['HTML', 'MongoDB', 'Javascript', 'Vue.js'],
+  projects: {
+    'ssafy-common': 'https://lab.ssafy.com/',
+    'ssafy-special': 'https://lab.ssafy.com/',
+    'ssafy-complete': 'https://lab.ssafy.com/',
+  },
+  tags: {
+    'Vue.js': 8,
+    React: 12,
+    Angular: 3,
+    Javascript: 24,
+  },
+  pinned_posts: [
+    {
+      id: 1,
+      username: 'Go SSAFY',
+      written_time: '2021-01-03T09',
+      like_num: 30,
+      comment_num: 5,
+      title: 'Vue interview questions and answers for js React developers',
+      tags: ['Vue.js', 'Javascript', 'React', 'Angular'],
+    },
+    {
+      id: 2,
+      username: 'Go SSAFY',
+      written_time: '2021-01-03T09',
+      like_num: 30,
+      comment_num: 5,
+      title: 'Vue interview Javascript questions and answers for js developers',
+      tags: ['Vue.js', 'Javascript', 'React', 'Angular'],
+    },
+    {
+      id: 3,
+      username: 'Go SSAFY',
+      written_time: '2021-01-03T09',
+      like_num: 30,
+      comment_num: 5,
+      title: 'interview Vue React  questions and answers for js developers',
+      tags: ['Vue.js', 'Javascript', 'React', 'Angular'],
+    },
+    {
+      id: 4,
+      username: 'Go SSAFY',
+      written_time: '2021-01-03T09',
+      like_num: 30,
+      comment_num: 5,
+      title: 'Vue  Angular interview questions and answers for js developers',
+      tags: ['Vue.js', 'Javascript', 'React', 'Angular'],
+    },
+  ],
+  posts: [
+    {
+      id: 1,
+      username: 'Go SSAFY',
+      written_time: '2021-01-03T09',
+      like_num: 30,
+      comment_num: 5,
+      title: 'Vue interview questions and answers for js developers',
+      tags: ['Vue.js', 'Javascript', 'React', 'Angular'],
+    },
+    {
+      id: 2,
+      username: 'Go SSAFY',
+      written_time: '2021-01-03T09',
+      like_num: 30,
+      comment_num: 5,
+      title: 'Vue interview questions and answers for js developers',
+      tags: ['Vue.js', 'Javascript', 'React', 'Angular'],
+    },
+    {
+      id: 3,
+      username: 'Go SSAFY',
+      written_time: '2021-01-03T09',
+      like_num: 30,
+      comment_num: 5,
+      title: 'Vue interview questions and answers for js developers',
+      tags: ['Vue.js', 'Javascript', 'React', 'Angular'],
+    },
+  ],
+  comments: [
+    {
+      id: 1,
+      post_id: 1,
+      username: 'Go SSAFY',
+      written_time: '2021-01-03T09',
+      title: 'Vue.js interview questions and answers for js developers',
+      comment:
+        'Hope this has been helpful and will give you ideas for your future.',
+    },
+    {
+      id: 1,
+      post_id: 2,
+      username: 'Go SSAFY',
+      written_time: '2021-01-03T09',
+      title: 'Vue.js interview questions and answers for js developers',
+      comment:
+        'Hope this has been helpful and will give you ideas for your future.',
+    },
+  ],
+};
+export { testCase };
