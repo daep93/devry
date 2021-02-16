676d2dbd8442a50ff491c9d8a420c9e98ebdd58f
daep93 <eoy1313@gmail.com>
2021-02-17 00:37:36 +0900

    [] fix/Router : update qnacreatepage path

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -44,7 +44,7 @@ const routes = [
   {
     path: '/qna/create',
     name: 'QnACreate',
-    component: () => import('@/views/common/QnaCreatePage.vue'),
+    component: () => import('@/views/qna/QnaCreatePage.vue'),
     beforeEnter: (to, from, next) => {
       if (!store.getters.isLogined) {
         alert('로그인이 필요합니다');
@@ -56,7 +56,7 @@ const routes = [
   {
     path: '/qna/:id',
     name: 'QnAUpdate',
-    component: () => import('@/views/common/QnaUpdatePage.vue'),
+    component: () => import('@/views/qna/QnaUpdatePage.vue'),
     beforeEnter: (to, from, next) => {
       if (!store.getters.isLogined) {
         alert('로그인이 필요합니다');
