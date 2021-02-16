b785db098cecb262cf3c098717a9068a73da615e
daep93 <eoy1313@gmail.com>
2021-02-16 14:55:18 +0900

    [S04P13B102-195] feat/ProfileSetting : Add tag auto-complete function

--- frontend/src/utils/tagColorMapper.js
+++ frontend/src/utils/tagColorMapper.js
@@ -6,7 +6,7 @@ const colorDict = alpha => {
     django: `rgba(16,62,46,${alpha})`,
     java: `rgba(255,143,143,${alpha})`,
     'vue.js': `rgba(65,184,131,${alpha})`,
-    typescript: `rgba(35,74,132,${alpha})`,
+    typescript: `rgba(49,120,198,${alpha})`,
     docker: `rgba(19,72,113,${alpha})`,
     python3: `rgba(30,56,187,${alpha})`,
     react: `rgba(97,218,246,${alpha})`,
