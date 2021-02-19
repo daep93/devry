c783b485480881ebd8621c388d12bf8a222ba2a3
daep93 <eoy1313@gmail.com>
2021-02-19 09:36:27 +0900

    [S04P13B102-250] feat/ForumDetailPage : Add thumbnail

--- frontend/src/views/forum/ForumDetailPage.vue
+++ frontend/src/views/forum/ForumDetailPage.vue
@@ -4,25 +4,37 @@
       <div class="row col-2 q-mt-lg">
         <div class="row col-9"></div>
         <div class="row col-3">
-          <forum-detail-status :info="status"></forum-detail-status>
+          <forum-detail-status
+            :info="status"
+            v-if="loaded"
+          ></forum-detail-status>
         </div>
       </div>
       <div class="row col-10 q-mt-lg">
         <div class="col-9">
           <div class="row col-12">
-            <forum-detail-content :info="forumBody"></forum-detail-content>
+            <forum-detail-content
+              :info="forumBody"
+              v-if="loaded"
+            ></forum-detail-content>
           </div>
         </div>
         <div class="col-3 q-pl-sm q-pr-xl">
-          <forum-short-profile :info="shortProfile"></forum-short-profile>
+          <forum-short-profile
+            :info="shortProfile"
+            v-if="loaded"
+          ></forum-short-profile>
         </div>
       </div>
     </div>
     <div class="row col-12">
-      <forum-comment :info="comments"></forum-comment>
+      <forum-comment :info="comments" v-if="loaded"></forum-comment>
     </div>
     <div class="row col-12 q-my-xl">
-      <forum-comment-create :info="createComments"></forum-comment-create>
+      <forum-comment-create
+        :info="createComments"
+        v-if="loaded"
+      ></forum-comment-create>
     </div>
   </div>
 </template>
@@ -50,6 +62,7 @@ export default {
       forumBody: '',
       comments: '',
       shortProfile: '',
+      loaded: false,
     };
   },
   async created() {
@@ -61,6 +74,7 @@ export default {
       this.forumBody = this.contents.forum_post[0];
       this.shortProfile = this.contents.writer_info[0];
       this.comments = this.contents.comments;
+      this.loaded = true;
     } catch (error) {
       console.log(error);
     }
