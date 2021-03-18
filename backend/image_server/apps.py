1515f775f715a76f9b39f517a81cde82eb863cf1
daep93 <eoy1313@gmail.com>
2021-03-18 19:20:46 +0900

    .

--- backend/image_server/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ImageServerConfig(AppConfig):
-    name = 'image_server'
--- backend/qnas/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Qna, Ans
-
-admin.site.register(Qna)
-admin.site.register(Ans)
-# Register your models here.
